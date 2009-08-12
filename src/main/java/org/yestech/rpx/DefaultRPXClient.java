bcedca3f1152570ea9580748a13582bb2cca2be5
Andrew Wright <ayax79@gmail.com>
2009-08-12 12:49:50 -0700

    fixed a compile error

--- src/main/java/org/yestech/rpx/DefaultRPXClient.java
+++ src/main/java/org/yestech/rpx/DefaultRPXClient.java
@@ -4,10 +4,13 @@ import org.jboss.resteasy.client.ClientRequest;
 import org.jboss.resteasy.client.ClientResponse;
 import org.yestech.rpx.objectmodel.AuthInfoResponse;
 
+import javax.ws.rs.core.MediaType;
+import java.awt.*;
+
 /**
  * @author A.J. Wright
  */
-public class DefaultRPXClient {
+public class DefaultRPXClient implements RPXClient {
 
     private static final String AUTH_INFO_URL = "https://rpxnow.com/api/v2/auth_info";
 
@@ -19,12 +22,14 @@ public class DefaultRPXClient {
 
     public AuthInfoResponse authInfo(String token) throws Exception {
         ClientRequest request = new ClientRequest(AUTH_INFO_URL);
+        //request.getProviderFactory().addMessageBodyReader();
+        request.accept(MediaType.APPLICATION_JSON_TYPE);
         request.setHttpMethod("get");
         request.getQueryParameters().add("token", token);
         request.getQueryParameters().add("apiKey", apiKey);
         ClientResponse<AuthInfoResponse> result = request.get(AuthInfoResponse.class);
         return result.getEntity();
     }
-
+                                                                                                                                                                                        
 
 }
--- src/main/java/org/yestech/rpx/RPXClient.java
+++ src/main/java/org/yestech/rpx/RPXClient.java
@@ -1,7 +1,10 @@
 package org.yestech.rpx;
 
+import org.yestech.rpx.objectmodel.AuthInfoResponse;
+
 /**
  * @author A.J. Wright
  */
 public interface RPXClient {
+    AuthInfoResponse authInfo(String token) throws Exception;
 }
