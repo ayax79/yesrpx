785a12d59cd8bea820e2e8c9b909abe5ec3aa72a
Andrew Wright <ayax79@gmail.com>
2009-08-12 23:57:12 -0700

    added a map method.

--- src/main/java/org/yestech/rpx/DefaultRPXClient.java
+++ src/main/java/org/yestech/rpx/DefaultRPXClient.java
@@ -2,6 +2,8 @@ package org.yestech.rpx;
 
 import org.yestech.rpx.objectmodel.AuthInfoResponse;
 import org.yestech.rpx.objectmodel.RPXException;
+import org.yestech.rpx.objectmodel.RPXStat;
+import static org.yestech.rpx.objectmodel.RPXUtil.jsonString;
 import org.json.JSONObject;
 import org.json.JSONException;
 import org.apache.commons.httpclient.HttpClient;
@@ -25,13 +27,13 @@ public class DefaultRPXClient implements RPXClient {
     public AuthInfoResponse authInfo(String token, boolean extended) throws IOException, JSONException, RPXException {
 
         StringBuilder url = new StringBuilder(RPX_API_URL);
-        url.append("auth_info?");
-        url.append("token=").append(token);
+        url.append("auth_info");
+        url.append("?token=").append(token);
         url.append("&apiKey=").append(apiKey);
         url.append("&extended=").append(extended);
 
         HttpClient client = new HttpClient();
-        GetMethod get = new GetMethod();
+        GetMethod get = new GetMethod(url.toString());
         client.executeMethod(get);
         String body = get.getResponseBodyAsString();
         JSONObject jo = new JSONObject(body);
@@ -40,6 +42,25 @@ public class DefaultRPXClient implements RPXClient {
 
         // If not continue on
         return AuthInfoResponse.fromJson(jo);
+    }
+
+    public RPXStat map(String identifier, String primaryKey, boolean overwrite) throws IOException, JSONException, RPXException {
+
+        StringBuilder url = new StringBuilder(RPX_API_URL);
+        url.append("map");
+        url.append("?identifier=").append(identifier);
+        url.append("&primaryKey=").append(primaryKey);
+        url.append("&overwrise=").append(overwrite);
+
+        HttpClient client = new HttpClient();
+        GetMethod get = new GetMethod(url.toString());
+        client.executeMethod(get);
+        String body = get.getResponseBodyAsString();
+        JSONObject jo = new JSONObject(body);
+        RPXException ex = RPXException.fromJSON(jo);
+        if (ex != null) throw ex;
+
+        return RPXStat.fromString(jsonString(jo, "stat"));
     }
 
 }
--- src/main/java/org/yestech/rpx/RPXClient.java
+++ src/main/java/org/yestech/rpx/RPXClient.java
@@ -1,10 +1,17 @@
 package org.yestech.rpx;
 
 import org.yestech.rpx.objectmodel.AuthInfoResponse;
+import org.yestech.rpx.objectmodel.RPXStat;
+import org.yestech.rpx.objectmodel.RPXException;
+import org.json.JSONException;
+
+import java.io.IOException;
 
 /**
  * @author A.J. Wright
  */
 public interface RPXClient {
     AuthInfoResponse authInfo(String token, boolean extended) throws Exception;
+
+    RPXStat map(String identifier, String primaryKey, boolean overwrite) throws IOException, JSONException, RPXException;
 }
