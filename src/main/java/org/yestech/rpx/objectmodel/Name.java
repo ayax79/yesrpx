56b9407beeb0f4949fb677401bdfba39578459ad
Andrew Wright <ayax79@gmail.com>
2009-08-12 21:07:06 -0700

    Added more fields.

--- src/main/java/org/yestech/rpx/objectmodel/Name.java
+++ src/main/java/org/yestech/rpx/objectmodel/Name.java
@@ -12,6 +12,11 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class Name {
 
     private String formatted;
+    private String familyName;
+    private String givenName;
+    private String middleName;
+    private String honorificPrefix;
+    private String honorificSuffix;
 
     public String getFormatted() {
         return formatted;
@@ -21,6 +26,46 @@ public class Name {
         this.formatted = formatted;
     }
 
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getHonorificPrefix() {
+        return honorificPrefix;
+    }
+
+    public void setHonorificPrefix(String honorificPrefix) {
+        this.honorificPrefix = honorificPrefix;
+    }
+
+    public String getHonorificSuffix() {
+        return honorificSuffix;
+    }
+
+    public void setHonorificSuffix(String honorificSuffix) {
+        this.honorificSuffix = honorificSuffix;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -28,27 +73,49 @@ public class Name {
 
         Name name = (Name) o;
 
-        //noinspection RedundantIfStatement
+        if (familyName != null ? !familyName.equals(name.familyName) : name.familyName != null) return false;
         if (formatted != null ? !formatted.equals(name.formatted) : name.formatted != null) return false;
+        if (givenName != null ? !givenName.equals(name.givenName) : name.givenName != null) return false;
+        if (honorificPrefix != null ? !honorificPrefix.equals(name.honorificPrefix) : name.honorificPrefix != null)
+            return false;
+        if (honorificSuffix != null ? !honorificSuffix.equals(name.honorificSuffix) : name.honorificSuffix != null)
+            return false;
+        //noinspection RedundantIfStatement
+        if (middleName != null ? !middleName.equals(name.middleName) : name.middleName != null) return false;
 
         return true;
     }
 
     @Override
     public int hashCode() {
-        return formatted != null ? formatted.hashCode() : 0;
+        int result = formatted != null ? formatted.hashCode() : 0;
+        result = 31 * result + (familyName != null ? familyName.hashCode() : 0);
+        result = 31 * result + (givenName != null ? givenName.hashCode() : 0);
+        result = 31 * result + (middleName != null ? middleName.hashCode() : 0);
+        result = 31 * result + (honorificPrefix != null ? honorificPrefix.hashCode() : 0);
+        result = 31 * result + (honorificSuffix != null ? honorificSuffix.hashCode() : 0);
+        return result;
     }
 
     @Override
     public String toString() {
         return "Name{" +
                 "formatted='" + formatted + '\'' +
+                ", familyName='" + familyName + '\'' +
+                ", givenName='" + givenName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", honorificPrefix='" + honorificPrefix + '\'' +
+                ", honorificSuffix='" + honorificSuffix + '\'' +
                 '}';
     }
 
     public static Name fromJson(JSONObject jo) {
         Name name = new Name();
         name.formatted = jsonString(jo, "formatted");
+        name.familyName = jsonString(jo, "familyName");
+        name.givenName = jsonString(jo, "givenName");
+        name.honorificPrefix = jsonString(jo, "honorificPrefix");
+        name.honorificSuffix = jsonString(jo, "honorificSuffix"); 
         return name;
     }
 }
